e5a5a94cfaa72c6d51b788eb1e3ad53da9771da9
Livan Jimenez <jimenezlivan14@gmail.com>
2019-04-03 23:53:31 -0400

    init

--- Arithmetic.java
+++ Arithmetic.java
@@ -2,140 +2,113 @@ import java.util.Stack;
 import java.util.EmptyStackException;
 import java.util.Scanner;
 
-class Arithmetic
-{
+class Arithmetic {
 	Stack stk;
-	String expression,
-			postfix;
+	String expression, postfix;
 	int length;
 
-	Arithmetic(String s)
-	{
+	Arithmetic(String s) {
 		expression = s;
-		postfix ="";
+		postfix = "";
 		length = expression.length();
 		stk = new Stack();
 	}
+
 	// Validate the expression - make sure parentheses are balanced
-	boolean isBalance()
-	{
+	boolean isBalance() {
 		// Already defined
 	}
 
 	// Convert expression to postfix notation
-	void postfixExpression()
-	{
+	void postfixExpression() {
 		stk.clear(); // Re-using the stack object
 		Scanner scan = new Scanner(expression);
 		char current;
 		// The algorithm for doing the conversion.... Follow the bullets
-		while (scan.hasNext())
-		{
+		while (scan.hasNext()) {
 			String token = scan.next();
 
 			if (isNumber(token)) // Bullet # 1
 				postfix = postfix + token + " ";
-			else
-			{
+			else {
 				current = token.charAt(0);
 
 				if (isParentheses(current)) // Bullet # 2 begins
 				{
-					if(stk.empty() || current == Constants.LEFT_NORMAL)
-					{
+					if (stk.empty() || current == Constants.LEFT_NORMAL) {
 
 						// push this element on the stack;
-					}
-					else if (current == Constants.RIGHT_NORMAL)
-					{
-						try
-						{
-						   /* Some details ... whatever is popped from the
- 						    * stack is an object, hence you must cast this
- 						    * object to its proper type, then extract its
- 						    * primitive data (type) value.
- 						    */
-							Character ch = (Character)stk.pop();
+					} else if (current == Constants.RIGHT_NORMAL) {
+						try {
+							/*
+							 * Some details ... whatever is popped from the stack is an object, hence you
+							 * must cast this object to its proper type, then extract its primitive data
+							 * (type) value.
+							 */
+							Character ch = (Character) stk.pop();
 							char top = ch.charValue();
 
-
-							while (top != Constants.LEFT_NORMAL)
-							{
-							 /* Append token popped onto the output string
-							  * Place at least one blank space between 
-							  * each token
-							*/
+							while (top != Constants.LEFT_NORMAL) {
+								/*
+								 * Append token popped onto the output string Place at least one blank space
+								 * between each token
+								 */
 							}
-						}
-						catch(EmptyStackException e)
-						{
+						} catch (EmptyStackException e) {
 
 						}
 					}
-				}// Bullet # 2 ends
+				} // Bullet # 2 ends
 				else if (isOperator(current))// Bullet # 3 begins
 				{
-					if(stk.empty())
+					if (stk.empty())
 						stk.push(new Character(current));
-					else
-					{
-						try
-						{
-							 // Remember the method peek simply looks at the top
- 							// element on the stack, but does not remove it.
+					else {
+						try {
+							// Remember the method peek simply looks at the top
+							// element on the stack, but does not remove it.
 
-							char top = (Character)stk.peek();
+							char top = (Character) stk.peek();
 							boolean higher = hasHigherPrecedence(top, current);
 
-							while(top != Constants.LEFT_NORMAL && higher)
-							{
+							while (top != Constants.LEFT_NORMAL && higher) {
 								postfix = postfix + stk.pop() + " ";
-								top = (Character)stk.peek();
+								top = (Character) stk.peek();
 							}
 							stk.push(new Character(current));
-						}
-						catch(EmptyStackException e)
-						{
+						} catch (EmptyStackException e) {
 							stk.push(new Character(current));
 						}
 					}
-				}// Bullet # 3 ends
+				} // Bullet # 3 ends
 			}
 		} // Outer loop ends
 
-		try
-		{
-			while(!stk.empty()) // Bullet # 4
-			;	// complete this
-		}
-		catch(EmptyStackException e)
-		{
+		try {
+			while (!stk.empty()) // Bullet # 4
+				; // complete this
+		} catch (EmptyStackException e) {
 
 		}
 	}
 
-	boolean isNumber(String str)
-	{
-		//define this method
+	boolean isNumber(String str) {
+		// define this method
 	}
 
-	boolean isParentheses(char current)
-	{
+	boolean isParentheses(char current) {
 		// define this method;
 	}
 
-	boolean isOperator(char ch)
-	{
+	boolean isOperator(char ch) {
 		// define this method
 	}
 
-	boolean hasHigherPrecedence(char top, char current)
-	{
+	boolean hasHigherPrecedence(char top, char current) {
 		// define this method
 	}
 
-	String getPostfix()
-	{
+	String getPostfix() {
 		// define method
 	}
 }
--- isBalance.java
+++ isBalance.java
@@ -1,53 +1,47 @@
 import java.util.EmptyStackException;
 import java.util.Stack;
 
-public class Arithmetic
-{
+public class isBalance {
 	private Stack<Object> stk;
 	private int length;
 	String expression;
-		    
-	public Arithmetic(String expression)
-	{
+
+	public isBalance(String expression) {
 		stk = new Stack<Object>();
 		this.expression = expression;
 		this.length = expression.length();
 	}
+
 	// Determine if parentheses are balanced
-	boolean isBalance()
-	{
+	boolean isBalance() {
 		int index = 0;
 		boolean fail = false;
-		
-		try
-		{
-			while (index < length && !fail)
-			{
+
+		try {
+			while (index < length && !fail) {
 				char ch = expression.charAt(index);
-				
-				switch (ch) 
-				{
-					case Constants.LEFT_NORMAL:
-						stk.push(new Character(ch));
+
+				switch (ch) {
+				case Constants.LEFT_NORMAL:
+					stk.push(new Character(ch));
 					break;
-					case Constants.RIGHT_NORMAL:
-						stk.pop();
+				case Constants.RIGHT_NORMAL:
+					stk.pop();
 					break;
-					default:
-						
+				default:
+
 					break;
-				}//end of swtich
+				}// end of swtich
 				index++;
-			}//end of while
-		}//end of try
-		catch (EmptyStackException e) 
-		{
-			System.out.println(e.toString()); 
+			} // end of while
+		} // end of try
+		catch (EmptyStackException e) {
+			System.out.println(e.toString());
 			fail = true;
 		}
 		if (stk.empty() && !fail)
 			return true;
 		else
 			return false;
-	} 
- }
+	}
+}
