b07df6af97267a713fd0440b4f0fe01ea8b8ee6c
Livan Jimenez <jimenezlivan14@gmail.com>
2019-04-05 14:57:45 -0400

    constants added

---
+++ Constants.java
@@ -0,0 +1,25 @@
+public class Constants {
+    public final static char LEFT_NORMAL = '(';
+
+    public final static char RIGHT_NORMAL = ')';
+
+    public final static char LEFT_CURLY = '{';
+
+    public final static char RIGHT_CURLY = '}';
+
+    public final static char LEFT_SQUARE = '[';
+
+    public final static char RIGHT_SQUARE = ']';
+
+    public final static char PLUS = '+';
+
+    public final static char MINUS = '-';
+
+    public final static char MULTIPLY = '*';
+
+    public final static char DIVIDE = '/';
+
+    public final static char MODULO = '%';
+
+    public final static char A_SPACE = ' ';
+}
